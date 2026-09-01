5b40667ecbea89fbdb370a46618b04e4bbc422ff
Starryboyjosh <isaula.joshua10@gmail.com>
2026-09-01 08:31:25 -0600

    Instalacion inicial de Laravel 12 (composer create-project laravel/laravel)

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <title>Inventario Mipymes</title>
+</head>
+<body>
+    <h1>Sistema de Inventario para Mipymes</h1>
+    <p><a href="{{ route('login') }}">Iniciar sesion</a></p>
+</body>
+</html>
